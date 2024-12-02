b76a222e36e395257107c1d6526040ee58943fda
CallumLewis - Promtek <callum.lewis@promtek.com>
2024-12-02 12:16:38 +0000

    Adds preview for height a and width in console POC for real time rendering

--- main.py
+++ main.py
@@ -1,116 +1,112 @@
 from PIL import Image
 import math
+import time
+import threading
+import os
 
 def characterDecide(gsValue):
     if gsValue < 35:
-        pixelChar = "#"
-
+        return "#"
     elif gsValue < 45:
-        pixelChar = "@"
-
+        return "@"
     elif gsValue < 55:
-        pixelChar = "%"
-
+        return "%"
     elif gsValue < 65:
-        pixelChar = "&"
-
+        return "&"
     elif gsValue < 80:
-        pixelChar = "$"
-
+        return "$"
     elif gsValue < 100:
-        pixelChar = "*"
-
+        return "*"
     elif gsValue < 115:
-        pixelChar = "+"
-
+        return "+"
     elif gsValue < 125:
-        pixelChar = "="
-
+        return "="
     elif gsValue < 140:
-        pixelChar = "~"
-
+        return "~"
     elif gsValue < 155:
-        pixelChar = "-"
-
+        return "-"
     elif gsValue < 165:
-        pixelChar = ";"
-
+        return ";"
     elif gsValue < 175:
-        pixelChar = "!"
-
+        return "!"
     elif gsValue < 185:
-        pixelChar = "?"
-
+        return "?"
     elif gsValue < 200:
-        pixelChar = ":"
-
+        return ":"
     elif gsValue < 215:
-        pixelChar = "."
-
+        return "."
     elif gsValue < 225:
-        pixelChar = ","
-
+        return ","
     elif gsValue < 235:
-        pixelChar = "'"
-
+        return "'"
     elif gsValue < 247:
-        pixelChar = "\""
-
+        return "\""
     else:
-        pixelChar = " "
-
-    return pixelChar
+        return " "
 
 def stringGenerator(characterArray, height, width):
-    string = ""
-    for y in range(height):  # Iterate over height first
-        for x in range(width):
-            string += characterArray[x][y]
-        string += "\n"
-    return string
+    output = []
+    for y in range(height):
+        line = "".join(characterArray[x][y] for x in range(width))
+        output.append(line)
+    return "\n".join(output)
 
 def writeToFile(filename, inputString):
-    filename = filename.split(".")[0]
-    filename = f"assets\ASCI\{filename}-ASCI.txt"
-    with open(filename, 'w') as file:
-        file.write(inputString)
+    while True:
+        print("\n" * 8)
+        print(inputString)
+        time.sleep(0.1)
+    #output_dir = "assets/ASCI"
+    #os.makedirs(output_dir, exist_ok=True)
+    #filename = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}-ASCI.txt")
+    #with open(filename, 'w') as file:
+        #file.write(inputString)
+    #print(f"Successfully wrote to {filename}")
 
-    print(f"Successfully wrote to {filename}")
+def graphicsProcessing(pixels, pixelsChar, width, height, xOffset, yOffset, xEnd, yEnd, contrast):
+    for y in range(yOffset, yEnd):
+        for x in range(xOffset, xEnd):
+            pixelRGB = pixels[x, y]
+            gsValue = int((0.299 * pixelRGB[0]) + (0.587 * pixelRGB[1]) + (0.114 * pixelRGB[2]))
+            gsValue = max(0, min(255, int(((gsValue - 125) * contrast) + 125)))
+            pixelsChar[x][y] = characterDecide(gsValue) + " "
 
-filename = input("Enter File name as string Including extensions such as png: ")
-pixel_density = int(input("Enter you pixel density for example 4 pixels becomes 1 you input would be 4: "))
-contrast = int(input("Enter Contrast: "))
+filename = "Donut.jpg"
+pixel_density = 64
+width = 48
+height = 24
+contrast = 1
+threads = 1
 
-image = Image.open(f"assets\images\{filename}")
-
+image = Image.open(f"assets/images/{filename}")
+image = image.resize((width, height))
 width, height = image.size
-
-image = image.resize((width // pixel_density, height // pixel_density))
-
-width, height = image.size
-
 pixels = image.load()
 
 pixelsChar = [[" " for _ in range(height)] for _ in range(width)]
 
-processed_pixels = 0
+xDiff = width // threads
+yDiff = height // threads
 
-for y in range(height):
-    for x in range(width):
-        pixelRGB = pixels[x, y]
-        gsValue = math.floor((0.299 * pixelRGB[0]) + (0.587 * pixelRGB[1]) + (0.114 * pixelRGB[2]))
-        gsValue = math.floor(((125 - gsValue) * contrast) + 125)
-        pixels[x, y] = (gsValue,gsValue,gsValue)
-        pixelsChar[x][y] = characterDecide(gsValue) + " "
+threadList = []
 
-        processed_pixels += 1
-        completion_percent = (processed_pixels / ( height * width)) * 100
+start_time = time.time()
 
-        if completion_percent % 1 == 0:
-            print(f"{completion_percent}% complete")
+for i in range(threads):
+    xOffset = xDiff * i
+    xEnd = xOffset + xDiff if i < threads - 1 else width
+    thread = threading.Thread(
+        target=graphicsProcessing,
+        args=(pixels, pixelsChar, width, height, xOffset, 0, xEnd, height, contrast)
+    )
+    threadList.append(thread)
+    thread.start()
 
+for thread in threadList:
+    thread.join()
 
-# Write to file using the corrected stringGenerator
+elapsed_time = time.time() - start_time
+print(f"{threads} threads - Time Taken: {elapsed_time} seconds")
 
-image.show()
-writeToFile(filename, stringGenerator(pixelsChar, height, width))
+ascii_art = stringGenerator(pixelsChar, height, width)
+writeToFile(filename, ascii_art)
